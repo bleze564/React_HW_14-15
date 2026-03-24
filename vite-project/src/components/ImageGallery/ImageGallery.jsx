eecacb98cafba8753754091848049f9d17e5009a
bleze564 <153006729+bleze564@users.noreply.github.com>
2026-03-24 20:37:23 +0200

    12312322

---
+++ vite-project/src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ImageGallery = () => {
+  return (
+    <div>
+      <ul class="gallery"></ul>
+    </div>
+  );
+};
+
+export default ImageGallery;
---
+++ vite-project/src/components/Modal/Modal.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <div>
+      <div class="overlay">
+        <div class="modal">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
